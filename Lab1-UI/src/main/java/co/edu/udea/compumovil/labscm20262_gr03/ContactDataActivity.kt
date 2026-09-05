26a5b74494c85f3b4cc3c750bc6f22e6cc0b1d2c
yeison8a35 <yeison8a35@gmail.com>
2026-09-05 12:16:41 -0500

    Implementa pantalla de datos de contacto

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/labscm20262_gr03/ContactDataActivity.kt
@@ -0,0 +1,349 @@
+package co.edu.udea.compumovil.labscm20262_gr03
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+
+
+class ContactDataActivity : ComponentActivity() {
+
+    private val viewModel: ContactDataViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ContactDataScreen(viewModel)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@androidx.compose.runtime.Composable
+fun ContactDataScreen(viewModel: ContactDataViewModel) {
+
+    val paisesLatinoamerica = listOf(
+        "Argentina",
+        "Bolivia",
+        "Brasil",
+        "Chile",
+        "Colombia",
+        "Costa Rica",
+        "Cuba",
+        "Ecuador",
+        "El Salvador",
+        "Guatemala",
+        "Honduras",
+        "México",
+        "Nicaragua",
+        "Panamá",
+        "Paraguay",
+        "Perú",
+        "República Dominicana",
+        "Uruguay",
+        "Venezuela"
+    )
+
+    val ciudadesColombia = listOf(
+        "Bogotá",
+        "Medellín",
+        "Cali",
+        "Barranquilla",
+        "Cartagena",
+        "Cúcuta",
+        "Bucaramanga",
+        "Pereira",
+        "Santa Marta",
+        "Ibagué",
+        "Manizales",
+        "Villavicencio",
+        "Pasto",
+        "Montería",
+        "Neiva",
+        "Armenia",
+        "Valledupar",
+        "Sincelejo",
+        "Popayán",
+        "Tunja"
+    )
+
+    val telefono by viewModel.telefono.collectAsState()
+    val direccion by viewModel.direccion.collectAsState()
+    val email by viewModel.email.collectAsState()
+    val pais by viewModel.pais.collectAsState()
+    val ciudad by viewModel.ciudad.collectAsState()
+    var mostrarErrores by remember { mutableStateOf(false) }
+    var paisExpandido by remember { mutableStateOf(false) }
+    var ciudadExpandida by remember { mutableStateOf(false) }
+
+    val telefonoFocus = remember { FocusRequester() }
+    val direccionFocus = remember { FocusRequester() }
+    val emailFocus = remember { FocusRequester() }
+    val paisFocus = remember { FocusRequester() }
+    val ciudadFocus = remember { FocusRequester() }
+
+    val teclado = LocalSoftwareKeyboardController.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .imePadding()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+
+        Text(
+            text = "Información de contacto"
+        )
+
+        OutlinedTextField(
+            value = telefono,
+            onValueChange = { viewModel.actualizarTelefono(it) },
+            label = { Text("Teléfono") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Phone,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    direccionFocus.requestFocus()
+                }
+            ),
+            isError = mostrarErrores && telefono.trim().length <7,
+            supportingText = {
+                if(mostrarErrores && telefono.trim().length < 7){
+                    Text("Ingresa un teléfono válido")
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(telefonoFocus)
+                .onFocusChanged {
+                    if (it.isFocused) {
+                        teclado?.show()
+                    }
+                },
+        )
+
+        OutlinedTextField(
+            value = direccion,
+            onValueChange = { viewModel.actualizarDireccion(it) },
+            label = { Text("Dirección") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next,
+                autoCorrectEnabled = false
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    emailFocus.requestFocus()
+                }
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(direccionFocus)
+        )
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { viewModel.actualizarEmail(it) },
+            label = { Text("Correo electrónico") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    paisFocus.requestFocus()
+                }
+            ),
+            isError = mostrarErrores &&
+                    !android.util.Patterns.EMAIL_ADDRESS
+                        .matcher(email.trim())
+                        .matches(),
+            supportingText = {
+                if(mostrarErrores &&
+                    !android.util.Patterns.EMAIL_ADDRESS
+                        .matcher(email.trim())
+                        .matches()
+                    ){
+                    Text("Ingresa un correo válido")
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(emailFocus)
+        )
+
+        ExposedDropdownMenuBox(
+            expanded = paisExpandido,
+            onExpandedChange = {
+                paisExpandido = !paisExpandido
+            }
+        ) {
+
+            OutlinedTextField(
+                value = pais,
+                onValueChange = {
+                    viewModel.actualizarPais(it)
+                    paisExpandido = true
+                },
+                label = { Text("País") },
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(
+                        expanded = paisExpandido
+                    )
+                },
+                isError = mostrarErrores && pais.isBlank(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = {
+                        ciudadFocus.requestFocus()
+                    }
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .menuAnchor()
+                    .focusRequester(paisFocus)
+            )
+
+            ExposedDropdownMenu(
+                expanded = paisExpandido,
+                onDismissRequest = {
+                    paisExpandido = false
+                }
+            ) {
+
+                paisesLatinoamerica
+                    .filter {
+                        it.contains(
+                            pais,
+                            ignoreCase = true
+                        )
+                    }
+                    .forEach { paisSeleccionado ->
+
+                        DropdownMenuItem(
+                            text = {
+                                Text(paisSeleccionado)
+                            },
+                            onClick = {
+                                viewModel.actualizarPais(
+                                    paisSeleccionado
+                                )
+                                paisExpandido = false
+                                ciudadFocus.requestFocus()
+                            }
+                        )
+                    }
+            }
+        }
+
+        ExposedDropdownMenuBox(
+            expanded = ciudadExpandida,
+            onExpandedChange = {
+                ciudadExpandida = !ciudadExpandida
+            }
+        ) {
+
+            OutlinedTextField(
+                value = ciudad,
+                onValueChange = {
+                    viewModel.actualizarCiudad(it)
+                    ciudadExpandida = true
+                },
+                label = { Text("Ciudad") },
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(
+                        expanded = ciudadExpandida
+                    )
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .menuAnchor()
+                    .focusRequester(ciudadFocus)
+            )
+
+            ExposedDropdownMenu(
+                expanded = ciudadExpandida,
+                onDismissRequest = {
+                    ciudadExpandida = false
+                }
+            ) {
+
+                ciudadesColombia
+                    .filter {
+                        it.contains(
+                            ciudad,
+                            ignoreCase = true
+                        )
+                    }
+                    .forEach { ciudadSeleccionada ->
+
+                        DropdownMenuItem(
+                            text = {
+                                Text(ciudadSeleccionada)
+                            },
+                            onClick = {
+                                viewModel.actualizarCiudad(
+                                    ciudadSeleccionada
+                                )
+                                ciudadExpandida = false
+                            }
+                        )
+                    }
+            }
+        }
+
+        Button(
+            onClick = {
+                mostrarErrores = !viewModel.datosValidos()
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(ciudadFocus)
+        ) {
+            Text("Continuar")
+        }
+    }
+}
+
--- Lab1-UI/src/main/java/co/edu/udea/compumovil/labscm20262_gr03/MainActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/labscm20262_gr03/MainActivity.kt
@@ -1,47 +1,61 @@
 package co.edu.udea.compumovil.labscm20262_gr03
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import co.edu.udea.compumovil.labscm20262_gr03.ui.theme.LabsCM20262Gr03Theme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             LabsCM20262Gr03Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen(
+                    onContactDataClick = {
+                        val intent = Intent(
+                            this,
+                            ContactDataActivity::class.java
+                        )
+                        startActivity(intent)
+                    }
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen(
+    onContactDataClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LabsCM20262Gr03Theme {
-        Greeting("Android")
+        Text(
+            text = "Laboratorio CM"
+        )
+
+        Button(
+            onClick = onContactDataClick
+        ) {
+            Text("Información de contacto")
+        }
     }
 }
+
